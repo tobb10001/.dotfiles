4e1c8a15bb1f965b2ed80b090adf1edeef75f26c
Tobias Fischer <tobb10001@gmail.com>
2022-12-28 23:06:11 +0100

    fix nvim leader

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,11 @@
 -- Include configs from other files
-require("plugins.main")
+require("options")
 
+require("plugins.main")
 require("plugins.nvim-cmp")
+
 require("keymaps")
 require("optics")
-require("options")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

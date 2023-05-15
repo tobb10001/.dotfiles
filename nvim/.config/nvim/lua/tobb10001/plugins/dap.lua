e0093617fde6a0d4b98ffd92e36ee57896ae6f9a
Tobias Fischer <tobb10001@gmail.com>
2023-05-15 23:06:57 +0200

    remove delve dap

--- nvim/.config/nvim/lua/tobb10001/plugins/dap.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/dap.lua
@@ -29,10 +29,7 @@ return {
 
         -- You'll need to check that you have the required things installed
         -- online, please don't ask me how to install them :)
-        ensure_installed = {
-          -- Update this to ensure that you have the debuggers for the langs you want
-          'delve',
-        },
+        ensure_installed = {},
       }
 
       -- Basic debugging keymaps, feel free to change to your liking!

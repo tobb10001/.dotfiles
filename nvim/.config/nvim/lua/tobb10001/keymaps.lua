a984fdbdcc396f57f7fa0dda435904a7704f9a9b
Tobias Fischer <tobb10001@gmail.com>
2023-05-15 22:45:54 +0200

    introduce actual file structure to nvim configuration

---
+++ nvim/.config/nvim/lua/tobb10001/keymaps.lua
@@ -0,0 +1,36 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Escape to escape from terminals.
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Rest.Nvim
+vim.keymap.set('n', '<leader>rr', require('rest-nvim').run, { desc = "Run the request under the cursor." })
+vim.keymap.set('n', '<leader>rp', function() require('rest-nvim').run(true) end,
+  { desc = "Preview the request cURL command." })
+vim.keymap.set('n', '<leader>rl', require('rest-nvim').last, { desc = "Re-run the last request." })
+
+-- Run a test file
+vim.keymap.set('n', '<leader>t', '<Plug>PlenaryTestFile')
+
+-- Type german letters
+vim.keymap.set('i', 'aee', 'ä')
+vim.keymap.set('i', 'oee', 'ö')
+vim.keymap.set('i', 'uee', 'ü')
+vim.keymap.set('i', 'Aee', 'Ä')
+vim.keymap.set('i', 'Oee', 'Ö')
+vim.keymap.set('i', 'Uee', 'Ü')
+vim.keymap.set('i', 'sss', 'ß')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
---
+++ nvim/.config/nvim/lua/tobb10001/plugins/cmp.lua
@@ -0,0 +1,13 @@
+-- Plugins related to completion
+
+return {
+  -- Autocompletion
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+  },
+}

4e2c8ab90d357e9499b2ee7c80915d2fbe10ae9f
Tobias Fischer <tobb10001@gmail.com>
2023-06-19 22:31:23 +0200

    get null-ls running

--- nvim/.config/nvim/lua/tobb10001/plugins/lsp.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/lsp.lua
@@ -11,7 +11,19 @@ return {
             {
                 'williamboman/mason-lspconfig.nvim',
                 config = true,
-            }
+            },
         },
+    },
+    {
+        'jose-elias-alvarez/null-ls.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        init = function()
+            null_ls = require('null-ls')
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.black,
+                },
+            })
+        end
     },
 }

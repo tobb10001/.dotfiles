582dce905412a3467a83c9a767f9ee94c343966a
Tobias Fischer <tobias.fischer@basf.com>
2023-07-07 16:37:50 +0200

    Add todo-comments.nvim

---
+++ nvim/.config/nvim/lua/tobb10001/plugins/todo-comments.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    keys = {
+        {
+            ']t',
+            function() require('todo-comments').jump_next() end,
+            mode = 'n',
+            desc = 'Jump to next TODO.',
+        },
+        {
+            '[t',
+            function() require('todo-comments').jump_prev() end,
+            mode = 'n',
+            desc = 'Jump to previous TODO.',
+        },
+    }
+}

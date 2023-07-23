dfb0d316d00dc0c6123fa11b94a9f8032d66ae2d
Tobias Fischer <tobb10001@gmail.com>
2023-07-23 13:46:37 +0200

    key to source lua files

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -3,3 +3,7 @@ vim.cmd(':set tabstop=4')
 vim.cmd(':set softtabstop=4')
 vim.cmd(':set shiftwidth=4')
 
+vim.keymap.set('n', '<leader>so', function ()
+    vim.cmd(':so %')
+end, { desc = '[So]urce the current file.' })
+

e6e94bcecb279728f8db66fe5c9684dd33bd977a
Tobias Fischer <tobias.fischer@basf.com>
2023-09-11 17:14:36 +0200

    activate TS highlighting

--- nvim/.config/nvim/lua/tobb10001/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/treesitter.lua
@@ -3,6 +3,14 @@ return {
         'nvim-treesitter/nvim-treesitter',
         dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         build = ':TSUpdate',
+        init = function()
+            require("nvim-treesitter.configs").setup({
+                highlight = {
+                    enable = true,
+                    disable = {},
+                },
+            })
+        end,
     },
     {
         'nvim-treesitter/playground',

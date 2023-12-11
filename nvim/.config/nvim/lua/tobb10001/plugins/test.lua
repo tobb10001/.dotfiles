25330b30130eb4a2b996323059fe0d770dbbb36a
Tobias Fischer <tobb10001@gmail.com>
2023-12-11 22:45:36 +0100

    add neotest

---
+++ nvim/.config/nvim/lua/tobb10001/plugins/test.lua
@@ -0,0 +1,42 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+
+        "nvim-neotest/neotest-go",
+    },
+    config = function()
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    return diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                end,
+            },
+        }, neotest_ns)
+        require("neotest").setup({
+            adapters = {
+                require("neotest-go"),
+            },
+        })
+    end,
+    keys = {
+        {
+            "<leader>tt",
+            function()
+                return require("neotest").run.run()
+            end,
+            mode = 'n',
+            desc = 'Run the nearest test',
+        },
+        {
+            "<leader>tf",
+            function()
+                return require("neotest").run.run(vim.fn.expand("%"))
+            end,
+            mode = 'n',
+            desc = 'Run current file',
+        }
+    }
+}

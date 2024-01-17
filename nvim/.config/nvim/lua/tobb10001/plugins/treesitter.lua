1ce1f2f74388a3d8e85d1093bd49b1803d26a31b
Tobias Fischer <tobb10001@gmail.com>
2024-01-17 21:20:12 +0100

    turbocharge treesitter

--- nvim/.config/nvim/lua/tobb10001/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/treesitter.lua
@@ -1,20 +1,115 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         build = ':TSUpdate',
         init = function()
             require("nvim-treesitter.configs").setup({
                 highlight = {
                     enable = true,
-                    disable = {},
+                    additional_vim_regex_highlighting = false,
                 },
+                indent = { enable = true },
+                ensure_installed = {
+                    "bash",
+                    "fish",
+                    "go",
+                    "html",
+                    "javascript",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "typescript",
+                    "yaml",
+                },
+                sync_install = false,
+                auto_install = true,
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "vin",
+                        node_incremental = "ni",
+                        scope_incremental = false, -- I don't understand scope.
+                        node_decremental = "nd",
+                    },
+                }
             })
         end,
+        tag = "*",
     },
     {
         'nvim-treesitter/playground',
         build = ':TSInstall query',
     },
-    tag = "v0.9.1"
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        init = function()
+            require('nvim-treesitter.configs').setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["aa"] = "@parameter.outer", -- ???
+                            ["ia"] = "@parameter.inner",
+                            ["ab"] = "@block.outer",
+                            ["ib"] = "@block.inner",
+                            ["ic"] = "@call.inner",
+                            ["oc"] = "@call.outer",
+                            ["ae"] = "@statement.outer", -- expression
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ii"] = "@conditional.inner", -- if
+                            ["ai"] = "@conditional.outer",
+                            ["il"] = "@loop.inner",
+                            ["al"] = "@loop.outer",
+                            ["as"] = "@class.outer",   -- struct
+                            ["is"] = "@class.inner",
+                            ["it"] = "@comment.inner", -- text
+                            ["at"] = "@comment.outer",
+                        }
+                    },
+                    swap = { enable = false }, -- for now
+                    move = {
+                        enable = true,
+                        set_jumps = false,
+                        goto_next_start = {
+                            ["]f"] = "@function.outer",
+                            ["]s"] = "@class.outer", -- struct
+                        },
+                        goto_next_end = {
+                            ["]F"] = "@function.outer",
+                            ["]S"] = "@class.outer", -- struct
+                        },
+                        goto_previous_start = {
+                            ["[f"] = "@function.outer",
+                            ["[s"] = "@class.outer", -- struct
+                        },
+                        goto_previous_end = {
+                            ["[F"] = "@function.outer",
+                            ["[S"] = "@class.outer", -- struct
+                        },
+                    },
+                    lsp_interop = {
+                        enable = true,
+                        peek_definition_code = {
+                            ["<leader>pf"] = "@function.outer",
+                            ["<leader>ps"] = "@class.outer",
+                        },
+                    }
+                }
+            })
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        init = function()
+            vim.keymap.set("n", "gC", function()
+                require('treesitter.context').go_to_context(vim.v.count1)
+            end, { silent = true, desc = "[G]oto [C]ontext" })
+        end
+    },
 }

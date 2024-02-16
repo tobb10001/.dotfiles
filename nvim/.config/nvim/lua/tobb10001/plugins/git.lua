ecbb12b6b9b1e3c32e9a124d50ee340163832833
Tobias Fischer <tobb10001@gmail.com>
2024-02-16 16:10:55 +0100

    stuff

--- nvim/.config/nvim/lua/tobb10001/plugins/git.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/git.lua
@@ -4,18 +4,31 @@ return {
         config = {
             on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
-                local map = function(mode, keys, func, desc)
-                    if desc then
-                        desc = 'Gitsigns: ' .. desc
+                local map = function(mode, keys, func, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    if opts.desc then
+                        opts.desc = 'GitSigns: ' .. opts.desc
                     end
-                    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+                    vim.keymap.set(mode, keys, func, opts)
                 end
-                map('n', '<leader>hs', gs.stage_hunk, "GitSigns: [S]tage [H]unk")
-                map('n', '<leader>hr', gs.reset_hunk, "GitSigns: [S]tage [R]eset")
+                map('n', '<leader>hs', gs.stage_hunk, { desc = "[S]tage [H]unk" })
+                map('n', '<leader>hr', gs.reset_hunk, { desc = "[S]tage [R]eset"})
                 map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                    "GitSigns: [S]tage [H]unk")
+                    { desc = "GitSigns: [S]tage [H]unk" })
                 map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                    "GitSigns: [S]tage [R]eset")
+                    { desc = "GitSigns: [S]tage [R]eset" })
+                map('n', ']h', function()
+                    if vim.wo.diff then return ']h' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, { expr=true, desc = "Next [H]unk" })
+
+                map('n', '[h', function()
+                    if vim.wo.diff then return '[h' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, { expr=true, desc = "Previous [H]unk" })
             end
         },
     },
@@ -48,4 +61,5 @@ return {
         build = function () require("gitlab.server").build(true) end,
         config = function() require("gitlab").setup() end,
     },
+    "sindrets/diffview.nvim",
 }

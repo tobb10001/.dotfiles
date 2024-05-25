def3dfff4c3fa66f02bb142003fcbbfaf3cb6118
Tobias Fischer <tobb10001@gmail.com>
2024-05-25 12:31:14 +0200

    emacs keybinds, fix lazy lock

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
     set -gx PATH ~/.local/bin $PATH
     set -gx PATH /usr/local/go/bin $PATH
     set -gx PATH ~/go/bin $PATH
-    set -gx PATH /usr/local/nvim-linux64/bin $PATH
     set -gx PATH ~/.nix-profile/bin/ $PATH
     set -gx XDG_CACHE_HOME "$HOME/.cache"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
@@ -36,37 +35,7 @@ if status is-interactive
     
     # Keybinds #########################################################################
 
-    fish_vi_key_bindings
-
-    # Enable !! and !$
-    # https://superuser.com/a/944589
-    function bind_bang
-        switch (commandline -t)[-1]
-            case "!"
-                commandline -t -- $history[1]
-                commandline -f repaint
-            case "*"
-                commandline -i !
-        end
-    end
-
-    function bind_dollar
-        switch (commandline -t)[-1]
-            case "!"
-                commandline -f backward-delete-char history-token-search-backward
-            case "*"
-                commandline -i '$'
-        end
-    end
-
-    # TODO: Get this stuff to work.
-    # function fish_user_key_bindings
-    #     bind ! bind_bang
-    #     bind '$' bind_dollar
-    # end
-    # bind \cK history_search_backward
-    # bind \cJ history_search_fowrard
-    # bind \cL accept_autosuggestion
+    fish_default_key_bindings
 
     # Init the stuff ###################################################################
 

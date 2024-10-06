20c7b790decf63058c587d52b9ca068c550b83d5
Tobias Fischer <tobb10001@gmail.com>
2024-10-06 17:56:41 +0200

    neotest

--- nvim/.config/nvim/lua/tobb10001/plugins/test.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/test.lua
@@ -1,22 +1,22 @@
 return {
-	-- {
-	-- 	"nvim-neotest/neotest",
-	-- 	dependencies = {
-	-- 		"nvim-neotest/nvim-nio",
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"antoinemadec/FixCursorHold.nvim",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		-- Adapters
-	-- 		"nvim-neotest/neotest-python",
-	-- 	},
-	-- 	config = function()
-	-- 		require("neotest").setup({
-	-- 			require("neotest-python")({
-	-- 				args = { "src/", "--log-level", "DEBUG" },
-	-- 			})
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			-- Adapters
+			"nvim-neotest/neotest-python",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-python")
+				}
+			})
+		end,
+	},
 	{
 		"andythigpen/nvim-coverage",
 		dependencies = { "nvim-lua/plenary.nvim" },

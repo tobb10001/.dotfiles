17458fcc7fe6c730ba16dfee946654b1b0e5bb7a
Tobias Fischer <tobb10001@gmail.com>
2025-04-17 17:07:09 +0200

    stuff

--- nvim/.config/nvim/lua/tobb10001/plugins/typescript.lua
+++ nvim/.config/nvim/lua/tobb10001/plugins/typescript.lua
@@ -1,8 +1,9 @@
 return {
 	"pmizio/typescript-tools.nvim",
-	ft = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+	ft = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" },
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	opts = {
+		filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 		settings = {
 			tsserver_file_preferences = {
 				includeInlayParameterNameHints = "all",
@@ -10,6 +11,9 @@ return {
 			tsserver_format_options = {
 				indentSize = 2,
 			},
+			tsserver_plugins = {
+				"@vue/typescript-plugin",
+			}
 		},
 	},
 	cond = not vim.g.vscode,

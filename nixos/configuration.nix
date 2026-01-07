97168d3d20b9d5ca617566742bc685cb57b9e54d
Tobias Fischer <tobb10001@gmail.com>
2026-01-07 16:29:56 +0100

    stuff

---
+++ nixos/configuration.nix
@@ -0,0 +1,243 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  inputs,
+  unstable,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+  ];
+
+  # Nix Settings
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = {
+    crypthome = {
+      device = "/dev/disk/by-uuid/534e0f18-eb05-4384-9c62-b2a3249c1eae";
+    };
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/mapper/crypthome";
+    fsType = "ext4";
+    options = [ "defaults" ];
+  };
+
+  # Kanata
+  boot.kernelModules = [ "uinput" ];
+  hardware.uinput.enable = true;
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+  users.groups.uinput = { };
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6epmtl";
+  };
+
+  networking.hostName = "Tobias-TB16G7"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "altgr-intl";
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tobi = {
+    isNormalUser = true;
+    description = "Tobi";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "input"
+      "uinput"
+    ];
+    packages = with pkgs; [
+      # Shell
+      direnv
+      eza
+      fzf
+      starship
+      zoxide
+
+      # Neovim
+      unstable.neovim
+      # Lazyvim Deps
+      luarocks
+      ast-grep
+      mermaid-cli
+      tectonic
+      tree-sitter
+      # Language Support
+      nixfmt
+      statix
+      tinymist
+
+      # CLI
+      bat
+      borgbackup
+      btop
+      delta
+      exiftool
+      fd
+      graphviz
+      grc
+      imagemagick
+      jq
+      lazygit
+      parallel-full
+      pciutils
+      ripgrep
+      stow
+      tldr
+      translate-shell
+      unstable.typst
+      usbutils
+      unstable.zola
+      yq
+
+      # Desktop
+      # discord
+      flameshot
+      fuzzel
+      kanata
+      kdePackages.gwenview
+      nextcloud-client
+      spotify
+      zotero
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    clang
+    fish
+    git
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    swaylock
+    unzip
+    wezterm
+    wget
+    xwayland-satellite
+  ];
+  environment.variables = {
+    TERM = "wezterm";
+  };
+
+  programs.niri.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+    enableHidpi = true;
+  };
+  services.displayManager.sddm.wayland.enable = true;
+
+  security.polkit.enable = true;
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "modesetting" ];
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+    ];
+  };
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
+  services.blueman.enable = true;
+  services.flatpak.enable = true;
+  # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+  programs.firefox.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
---
+++ nixos/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    unstablepkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "unstablepkgs";
+    };
+  };
+  outputs = inputs@{ self, nixpkgs, unstablepkgs, ... }:
+  let
+    system = "x86_64-linux";
+    unstable = import unstablepkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations."Tobias-TB16G7" = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs unstable; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
